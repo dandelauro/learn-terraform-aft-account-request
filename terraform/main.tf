69b40941e4bb13cb40facea3b6da7ad1ff6db410
dan delauro <dan.delauro@wexinc.com>
2023-02-28 09:07:34 -0500

    Delete account - AFT Sandbox 100

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
-module "aft_sandbox_100" {
-  source = "./modules/aft-account-request"
+# module "aft_sandbox_100" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "aws_admin_master_nonprod+aft_sandbox_100@wexinc.com"
-    AccountName               = "AFT Sandbox 100"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "aws_admin_master_nonprod+aft_controller_nonprod@wexinc.com"
-    SSOUserFirstName          = "AFT"
-    SSOUserLastName           = "Sandbox 100"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "aws_admin_master_nonprod+aft_sandbox_100@wexinc.com"
+#     AccountName               = "AFT Sandbox 100"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "aws_admin_master_nonprod+aft_controller_nonprod@wexinc.com"
+#     SSOUserFirstName          = "AFT"
+#     SSOUserLastName           = "Sandbox 100"
+#   }
 
-  account_tags = {
-    "AFT" = "Sandbox 101"
-  }
+#   account_tags = {
+#     "AFT" = "Sandbox 101"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "Dan DeLauro"
-    change_reason       = "Testing AFT account request."
-  }
+#   change_management_parameters = {
+#     change_requested_by = "Dan DeLauro"
+#     change_reason       = "Testing AFT account request."
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
